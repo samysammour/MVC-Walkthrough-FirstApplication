713a0107d8ce460ba315a57ba6126ca5b6568996
ssammour <ssammour@NB-000608>
2017-03-19 17:55:20 +0100

    Add project files.

---
+++ FirstApplication/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstApplication.Models;
+
+namespace FirstApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Person person1 = new Person();
+            person1.Id = 1;
+            person1.Name = "Alice";
+
+            Person person2 = new Person {Id = 2, Name = "John"};
+
+            List<Person> list = new List<Person>();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
